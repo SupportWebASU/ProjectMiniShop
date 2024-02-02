960c0a3b130052d1598bea6eb4c92b08c40049bc
KerolosAminFahmy <kerolosamenfahmy@gmail.com>
2024-02-02 21:52:20 +0200

    Add project files.

---
+++ ProjectMiniShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjectMiniShop.Models;
+using System.Diagnostics;
+
+namespace ProjectMiniShop.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
