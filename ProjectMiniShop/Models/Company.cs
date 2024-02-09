5db2a410bbcee1416cb80daf199c317b9dda3fb4
Matar <mhmdmatar340@gmail.com>
2024-02-09 15:58:04 +0200

    Session 2 - Add DataAnotations For product and company

--- ProjectMiniShop/Models/Company.cs
+++ ProjectMiniShop/Models/Company.cs
@@ -1,8 +1,12 @@
-﻿namespace ProjectMiniShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectMiniShop.Models
 {
     public class Company
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
