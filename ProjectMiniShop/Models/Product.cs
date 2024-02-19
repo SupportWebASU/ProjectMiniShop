e8cf8b42f07a33590fb7c86fe0ddc33b1c889402
AhmedAdel555 <ahmedbassiouni555@gmail.com>
2024-02-19 22:09:34 +0200

    add built-in validation

--- ProjectMiniShop/Models/Product.cs
+++ ProjectMiniShop/Models/Product.cs
@@ -11,17 +11,26 @@ namespace ProjectMiniShop.Models
         public int Id { get; set; }
 
         [Required]
+        [DeniedValues("AAA", "BBB")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Characters are not allowed.")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "description is required")]
+        [Length(5, 50)]
+        [RegularExpression(@"^[a-zA-Z' '-'\s]{1,40}$",
+   ErrorMessage = "Characters are not allowed.")]
         public string Description { get; set; }
 
         [Required]
+        [Range(1, 50000, ErrorMessage = "please enter price from 1 to 50000")]
         public float Price { get; set; }
 
-        public int? Quantity { get; set; }
+        [Required]
+        [Range(1, 3000, ErrorMessage = "please enter Quantity from 1 to 3000")]
+        public int Quantity { get; set; }
 
-        public bool EnableSize {  get; set; }
+        public bool EnableSize { get; set; }
         public int CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         public Company? Company { get; set; }
