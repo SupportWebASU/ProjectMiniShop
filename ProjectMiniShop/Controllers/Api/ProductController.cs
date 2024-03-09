09e36524417cac59328c1c614932e0b49024a457
KerolosAminFahmy <kerolosamenfahmy@gmail.com>
2024-03-09 16:28:21 +0200

    Session 8 - CRUD Operation With API

---
+++ ProjectMiniShop/Controllers/Api/ProductController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectMiniShop.Data;
+using ProjectMiniShop.Models;
+
+namespace ProjectMiniShop.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(ApplicationDbContext _db) : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<List<Product>> getProduct()
+        {
+            var products = _db.Products.Include(m => m.Company).ToList();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Product> getProduct(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var products = _db.Products.Include(m => m.Company).SingleOrDefault(m=>m.Id==id);
+            if (products == null)
+                return NotFound();
+            return Ok(products);
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            _db.Products.Add(product);
+            _db.SaveChanges();
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var products = _db.Products.Include(m => m.Company).
+                SingleOrDefault(m => m.Id == id);
+            if (products == null)
+                return NotFound();
+            _db.Products.Remove(products);
+            _db.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateProduct(Product Updatedproduct)
+        {
+            if (Updatedproduct.Id <= 0 && !ModelState.IsValid)
+            {
+                return BadRequest();
+                
+            }
+            var product=_db.Products.Include(m => m.Company).FirstOrDefault(m=>m.Id== Updatedproduct.Id);
+            if (product == null)
+                return NotFound();
+            product.Name = Updatedproduct.Name;
+            product.Description = Updatedproduct.Description;
+            product.Price = Updatedproduct.Price;
+            product.EnableSize = Updatedproduct.EnableSize;
+            product.CompanyId = Updatedproduct.CompanyId;
+            _db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ ProjectMiniShop/ViewModel/ProductViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ProjectMiniShop.Models;
+
+namespace ProjectMiniShop.ViewModel
+{
+    public class ProductViewModel
+    {
+        public  Product Product { get; set; }
+        public List<Company> companies { get; set; }
+    }
+}
